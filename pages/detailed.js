6762c3acde52babf1f8d33f4be8dc36b9565c48e
请不要阻拦我学习 <924370430@qq.com>
2020-12-02 12:40:45 +0800

    文章详细页面markdown解析

--- pages/detailed.js
+++ pages/detailed.js
@@ -2,7 +2,7 @@
  * @Author: DaZheng
  * @Date: 2020-12-01 16:00:49
  * @LastEditors: g05047
- * @LastEditTime: 2020-12-02 12:25:45
+ * @LastEditTime: 2020-12-02 12:38:52
  * @Description: file content
  */
 /* 文章详情页 */
@@ -13,6 +13,7 @@ import Author from '../components/Author'
 import Advert from '../components/Advert'
 import Footer from '../components/Footer'
 import '../static/style/pages/detailed.css'
+import ReactMarkdown from 'react-markdown'
 
 import {
   HistoryOutlined,
@@ -21,6 +22,41 @@ import {
 } from '@ant-design/icons';
 
 export default function Detailed() {
+  
+  let markdown='# P01:课程介绍和环境搭建\n' +
+  '[ **M** ] arkdown + E [ **ditor** ] = **Mditor**  \n' +
+  '> Mditor 是一个简洁、易于集成、方便扩展、期望舒服的编写 markdown 的编辑器，仅此而已... \n\n' +
+  '**这是加粗的文字**\n\n' +
+  '*这是倾斜的文字*`\n\n' +
+  '***这是斜体加粗的文字***\n\n' +
+  '~~这是加删除线的文字~~ \n\n'+
+  '\`console.log(111)\` \n\n'+
+  '# p02:来个Hello World 初始Vue3.0\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n'+
+  '***\n\n\n' +
+  '# p03:Vue3.0基础知识讲解\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n\n'+
+  '# p04:Vue3.0基础知识讲解\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n\n'+
+  '#5 p05:Vue3.0基础知识讲解\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n\n'+
+  '# p06:Vue3.0基础知识讲解\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n\n'+
+  '# p07:Vue3.0基础知识讲解\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n\n'+
+  '``` var a=11; ```'
   return (
     <div>
       <Head>
@@ -49,7 +85,11 @@ export default function Detailed() {
                   <span><FireOutlined />5900</span>
               </div>
               <div className="detailed-content">
-                markDown 内容
+                <ReactMarkdown 
+                  source={markdown}
+                  escapeHtml={false}
+                  
+                />
               </div>
             </div>
           </div>
